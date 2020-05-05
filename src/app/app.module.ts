4ffedef8eb1332fdedd43e7d78f8828ea17f07d9
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-05-05 07:22:13 +0300

    Adding more component for demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warnings/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './warnings/success-alert/success-alert.component';
+import { DangerAlertComponent } from './danger-alert/danger-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    DangerAlertComponent
   ],
   imports: [
     BrowserModule,
