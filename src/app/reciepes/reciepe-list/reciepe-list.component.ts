fad94e027730b3857a1e4ffb3a9ee21676de6bbf
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-05-15 18:09:47 +0300

    Adding first reciepe list

--- src/app/reciepes/reciepe-list/reciepe-list.component.ts
+++ src/app/reciepes/reciepe-list/reciepe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Reciepe } from '../reciepe.model';
+
 @Component({
   selector: 'app-reciepe-list',
   templateUrl: './reciepe-list.component.html',
   styleUrls: ['./reciepe-list.component.css']
 })
 export class ReciepeListComponent implements OnInit {
+
+  reciepes: Reciepe[] = [
+    new Reciepe('A Test Reciepe', 'This is a simple reciepe test', './home/joasjaphes/Pictures/key_to_success.png')
+  ];
 
   constructor() { }
 
--- src/app/reciepes/reciepe.model.ts
+++ src/app/reciepes/reciepe.model.ts
@@ -3,7 +3,7 @@ export class Reciepe {
   public description: string;
   public imagePath: string;
 
-  Reciepe(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
