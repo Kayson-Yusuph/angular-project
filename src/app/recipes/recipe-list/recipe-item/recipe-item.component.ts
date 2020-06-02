6401442d0dd9cbfd365be03d5d045235648607ee
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-02 10:54:31 +0300

    Passing down recipe to the show detail component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -10,9 +10,14 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
 
   @Input('recipeItem') recipe: Recipe;
+  @Output() showItemDetails = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void { }
+
+  showDetails(item: Recipe) {
+    this.showItemDetails.emit(item);
+  }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -11,12 +11,19 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A Test recipe', 'This is a simple recipe test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg'),
-    new Recipe('A Test recipe', 'This is a simple recipe test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg')
+    new Recipe('A New recipe', 'This is a next recipe test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg')
   ];
+
+  detailedRecipe: Recipe;
+  @Output() showItemDetails = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onShowRecipeDetails(item: Recipe) {
+    this.showItemDetails.emit(item);
   }
 
 }
