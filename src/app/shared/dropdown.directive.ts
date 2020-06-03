52919a55a523935d6206e969f8e79a8368076365
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-03 10:56:01 +0300

    Adding drop down directictive to menu

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  clicked = false;
+  @HostListener('click') click() {
+    if (this.clicked) {
+      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+      this.clicked = false;
+    } else {
+      this.renderer.addClass(this.elementRef.nativeElement, 'open');
+      this.clicked = true;
+    }
+  }
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
 }
