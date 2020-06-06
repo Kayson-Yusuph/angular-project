600c05008d7ac61d307b35880f30ef7054cda5c2
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-06 21:23:20 +0300

    Modifying how to fetch recipe detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const appRoutes: Routes = [
   {
     path: 'recipes', component: RecipesComponent, children: [
       { path: '', component: RecipeStartComponent },
-      { path: ':id/details', component: RecipeDetailComponent },
+      { path: ':id', component: RecipeDetailComponent },
       { path: ':id/edit', component: RecipeEditComponent },
     ]
   },
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -33,7 +33,7 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   editRecipe() {
-    this.router.navigate(['../', 'edit'], {relativeTo: this.route});
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
   deleteRecipe() {
