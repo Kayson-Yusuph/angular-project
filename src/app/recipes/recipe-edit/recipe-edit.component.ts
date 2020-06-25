62c67a5eb178644daacb47aa1a14acc1d3249037
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-25 07:20:42 +0300

    Synchronizing form group to the html

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
-import { RecipeService } from 'src/app/services/recipes.service';
+
+import { RecipeService } from '../../services/recipes.service';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,19 +11,39 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-  recipe: Recipe;
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
+      console.log('Called!', params);
       if (params.id !== undefined) {
         this.id = +params.id;
-        this.recipe = this.recipeService.getRecipe(this.id);
         this.editMode = true;
+        this.initForm();
       }
+    });
+  }
+
+  private initForm() {
+    let name = '';
+    let imagePath = '';
+    let description = '';
+    if (this.editMode) {
+      console.log('In form init!');
+      const recipe = this.recipeService.getRecipe(this.id);
+      console.log({recipe});
+      name = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(name),
+      imagePath: new FormControl(imagePath),
+      description: new FormControl(description)
     });
   }
 
