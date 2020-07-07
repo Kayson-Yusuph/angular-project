7b4ee1951335b45c88f4920464b569e8aa7450b1
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-07 05:55:17 +0300

    Fixing some issue with recipe ingredients after loading

--- src/app/services/data-store.service.ts
+++ src/app/services/data-store.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from './recipes.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -9,24 +10,30 @@ export class DataStoreService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService
-  ) {}
+  ) { }
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http.put('https://angular-app-268d0.firebaseio.com/recipes.json', recipes)
-        .subscribe((res) => {
-          console.log(res);
-        },(error) => {
-          console.error('Failed to save data: ' + error.message);
-        });
+      .subscribe((res) => {
+        console.log(res);
+      }, (error) => {
+        console.error(error);
+      });
   }
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://angular-app-268d0.firebaseio.com/recipes.json')
+      .pipe(map(res => {
+        return res.map(rsp => {
+          const ingredients = rsp.ingredients ? rsp.ingredients : [];
+          return { ...rsp, ingredients };
+        });
+      }))
       .subscribe((res) => {
         this.recipeService.setRecipes(res);
       }, (error) => {
-        console.error('Failed to fetch data: ' + error.message);
+        console.error(error);
       });
   }
 }
