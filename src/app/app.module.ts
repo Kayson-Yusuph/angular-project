48586fb3eb2e892be4e7ef8eabe3d0d0d2d1b881
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-15 06:03:34 +0300

    Adding Recipes separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,41 +4,29 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { HeaderComponent } from './header/header.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './services/shopping-list.service';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ErrorPageComponent } from './error-page/error-page.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeService } from './services/recipes.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoaderComponent } from './shared/loader/loader.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceHolderDirective } from './shared/placeholder.directive';
-import { Alert } from 'selenium-webdriver';
+import { RecipeService } from './services/recipes.service';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent,
     ShoppingListComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
     ShoppingListEditComponent,
     HeaderComponent,
     DropdownDirective,
-    RecipeEditComponent,
     ErrorPageComponent,
-    RecipeStartComponent,
     AuthComponent,
     LoaderComponent,
     AlertComponent,
@@ -49,7 +37,9 @@ import { Alert } from 'selenium-webdriver';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    // app modules
+    AppRoutingModule,
+    RecipesModule
   ],
   // entryComponents: [
   //   AlertComponent
