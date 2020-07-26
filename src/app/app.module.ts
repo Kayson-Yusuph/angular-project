1af96b4fa832c1c5247410906efae4dde7f084bd
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-26 09:07:55 +0300

    Adding store module and actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { CoreModule } from './core.module';
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     // app modules
     SharedModule,
     CoreModule,
