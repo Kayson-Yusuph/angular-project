f35e47284df0ce5c12a76c76d9062cf0856e53bd
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-09-01 14:21:06 +0300

    Fixing issue with resolver when reloading with recipe selected

--- src/app/services/recipes-resolver.service.ts
+++ src/app/services/recipes-resolver.service.ts
@@ -5,18 +5,29 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { take } from 'rxjs/operators';
+import { take, map, switchMap } from 'rxjs/operators';
 import { Actions, ofType } from '@ngrx/effects';
 
 import { Recipe } from '../recipes/recipe.model';
 import { AppState } from '../store/app.reducers';
 import * as RecipeActions from '../recipes/store/recipe.actions';
+import { of } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(private store: Store<AppState>, private actions$: Actions) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    this.store.dispatch(new RecipeActions.FetchRecipes());
-    return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+    return this.store.select('recipe').pipe(
+      take(1),
+      map((recipeState) => recipeState.recipes),
+      switchMap((recipes) => {
+        if (recipes.length) {
+          return of(recipes);
+        } else {
+          this.store.dispatch(new RecipeActions.FetchRecipes());
+          return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+        }
+      })
+    );
   }
 }
